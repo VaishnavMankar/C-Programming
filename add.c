92454c835b4feb6fa02549f6c0b4b55f8623567a
VaishnavMankar <vmankar2910@gmail.com>
2024-08-06 19:01:09 +0530

    addition code

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter integer (a) : ");
+    scanf("%d",&a);
+    
+    printf("Enter integer (b) : ");
+    scanf("%d",&b);
+
+    printf("Sum of %d and %d is %d",a,b,a+b);
+    return 0;
+}
