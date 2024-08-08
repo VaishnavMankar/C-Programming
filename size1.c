364d0b275ab1931bd18dcf580ceda370cd1f51a4
VaishnavMankar <vmankar2910@gmail.com>
2024-08-08 22:44:58 +0530

    initial commit

---
+++ size1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("%lu",sizeof(int));
+    printf("%lu",sizeof(long));
+    printf("%lu",sizeof(void));
+    printf("%lu",sizeof(float));
+    return 0;
+}
