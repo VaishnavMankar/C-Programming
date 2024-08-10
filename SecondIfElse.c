da0d49713f2c128c98b52e4b76d7e7ce5a35c3ca
VaishnavMankar <vmankar2910@gmail.com>
2024-08-10 09:59:37 +0530

    Greatest number

---
+++ SecondIfElse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter your integer: \n");
+    scanf("%d", &a);
+
+    if(a < 100){
+        printf("%d is a small number", a);
+    }else if(a > 100 && a < 1000){
+        printf("%d is a big number",a);
+    }
+    else{
+        printf("%d is the largest number",a);
+    }
+}
