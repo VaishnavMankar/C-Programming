c9c31815d08334fc210e08544f5bf15265951774
Vaishnav Mankar <vmankar2910@gmail.com>
2024-08-14 11:55:00 +0530

    Mathematical calculation

---
+++ Print_4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter number 1 = \n");
+    scanf("%d",&a);
+    printf("Enter number 2 = \n");
+    scanf("%d", &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d x %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+    
+}
