cce81d8bafee2e5b213af20cb24f3179f93c6981
VaishnavMankar <vmankar2910@gmail.com>
2024-08-18 14:11:30 +0530

    Find the number is positive or negative

---
+++ IfElse_2_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a ;
+    printf("Enter an number = \n");
+    scanf("%d",&a);
+    if(a > 0){
+        printf("%d ia a positive numbe\n",a);
+    }
+    else{
+        printf("%d is a negative nnumber\n",a);
+    }
+    return 0;
+}
