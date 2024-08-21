46cfafc3da150c059440c06c052258182abed91c
Vaishnav Mankar <vmankar2910@gmail.com>
2024-08-21 17:32:02 +0530

    Create a  loop that prints Welcome once.

---
+++ Assignment_1_3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    do{
+        printf("Welcom!\n");
+    }
+    while(i == 1);
+    return 0;
+}
+
+// #include <stdio.h>
+
+// int main() {
+//     do {
+//         printf("Welcome\n"); // Print "Welcome" once
+//     } while (0); // The condition is false, so the loop will not repeat
+
+//     return 0; // Return 0 to indicate successful execution
+// }
