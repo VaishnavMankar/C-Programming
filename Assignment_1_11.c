af8bf372ae6cb4dc80e5d5e14af5207694e04638
Vaishnav Mankar <vmankar2910@gmail.com>
2024-08-26 23:07:43 +0530

    Addition of even number ffrom 1 to 50

---
+++ Assignment_1_11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int sum = 0;
+    for(int i = i; i <= 50; i++){
+        if(i % 2 == 0){
+            sum += i;
+        }
+    }
+    printf("The sum of the number fro 1 to 50 ehich are even is %d\n",sum);
+    return 0;
+}
+// waiting to get upload n github
