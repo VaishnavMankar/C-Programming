026d30c9cc923e7d6434fa3f386ed9e9a0636752
Vaishnav Mankar <vmankar2910@gmail.com>
2024-08-29 20:05:32 +0530

    calculate

--- Assignment_1_12.c
+++ Assignment_1_12.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 int main()
 {
-     int a,b,result,n,i;
-     printf("Enter the number of terms: \n");
-     scanf("%d",&n);
+     int a,b,result,n = 10,i;
+    //  printf("Enter the number of terms: \n");
+    //  scanf("%d",&n);
 
      a = 0;
      b = 1;
-     fot(i = 1; i <= n; i++)
+     for(i = 1; i <= n; i++)
      {
-        printf("%d",a);
+        //printf("%d",a);
         result = a + b;
         a = b;
         b = result;
+        // printf("%d",a);
      }
-     printf("\n");
+     printf("%d",a);
      return 0;
 }
