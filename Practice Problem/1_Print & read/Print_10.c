89379431662b2bdc1e6caa69233d8f3735c5d9f7
VaishnavMankar <vmankar2910@gmail.com>
2024-09-29 10:14:47 +0530

    Program to read a character from a user, assign it to a variable and print it.

---
+++ Practice Problem/1_Print & read/Print_10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char Character;
+
+    printf("Enter the character you want to print\n");
+    scanf("%c",&Character);
+
+    printf("The character you want is: %c\n",Character);
+    return 0;
+}
