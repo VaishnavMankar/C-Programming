2a87d84f60c393d1292149073d515a9777a28336
VaishnavMankar <vmankar2910@gmail.com>
2024-09-30 22:30:46 +0530

    Read five integers from the user and print them.

---
+++ Practice Problem/3_1D & 2D Array/Section A/A_Array_1.c
@@ -0,0 +1,24 @@
+// Read five integers from the user and print them.
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,f;
+    printf("Enter the first integer: \n");
+    scanf("%d",&a);
+    printf("Enter the second integer: \n");
+    scanf("%d",&b);
+    printf("Enter the third integer: \n");
+    scanf("%d",&c);
+    printf("Enter the fourth integer: \n");
+    scanf("%d",&d);
+    printf("Enter the fifth integer: \n");
+    scanf("%d",&f);
+    
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("%d \n", d);
+    printf("%d \n", f);
+
+    return 0;
+}
