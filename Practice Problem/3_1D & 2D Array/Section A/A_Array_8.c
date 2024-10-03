ddcd05d0de1a65e9c194dda6e212c8ccc2674d36
VaishnavMankar <vmankar2910@gmail.com>
2024-10-03 11:38:06 +0530

    Read a character from the user and print it.

---
+++ Practice Problem/3_1D & 2D Array/Section A/A_Array_8.c
@@ -0,0 +1,10 @@
+// Read a character from the user and print it.
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter an characher\n");
+    scanf("%c",&a);
+    printf("the character you entered is %c\n",a);
+    return 0;
+}
