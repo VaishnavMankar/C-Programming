3f0fc6248cdb547a47ff2190ccec8980b6aede32
VaishnavMankar <vmankar2910@gmail.com>
2024-10-25 08:57:49 +0530

    Add files via upload

---
+++ Exams/Test4/Que_h.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Size of int is %lu\n",sizeof(int));
+    printf("Size of float is %lu\n",sizeof(float));
+    printf("Size of char is %lu\n",sizeof(char));
+    return 0;
+}
