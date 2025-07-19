dfedf77ab36a8115345857c4720ea947d27f18c1
Vaishnav Mankar <vmankar2910@gmail.com>
2025-07-19 23:58:31 +0530

    Sum of Digits in a Number

---
+++ Practice Problem/4_Loops/WhileLoop/WhileLoop_6.c
@@ -0,0 +1,19 @@
+// 6. Sumof Digits in a Number
+
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int temp = num;
+    while (temp > 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+
+    printf("Sum of digits = %d\n", sum);
+    return 0;
+}
