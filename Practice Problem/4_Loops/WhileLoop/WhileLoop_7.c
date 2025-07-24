bc783627710375e7b8e4ebc8085725a2c2451b9f
Vaishnav Mankar <vmankar2910@gmail.com>
2025-07-24 11:34:55 +0530

    Print Squares of Numbers from 1 to N

---
+++ Practice Problem/4_Loops/WhileLoop/WhileLoop_7.c
@@ -0,0 +1,16 @@
+// 7. Print Squares of Numbers from 1 to N
+
+#include <stdio.h>
+ 
+int main() {
+    int i = 1, N;
+    printf("Enter a number: ");
+    scanf("%d", &N);
+
+    while (i <= N) {
+        printf("Square of %d is %d\n", i, i * i);
+        i++;
+    }
+
+    return 0;
+}
