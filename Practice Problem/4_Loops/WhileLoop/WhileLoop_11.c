3ca2ba90feb358ae5e7bafb8fa426f843a9635ec
Vaishnav Mankar <vmankar2910@gmail.com>
2025-08-03 16:07:06 +0530

    Find the Sum of Odd Digits in a Number

---
+++ Practice Problem/4_Loops/WhileLoop/WhileLoop_11.c
@@ -0,0 +1,20 @@
+// 11. Find the Sum of Odd Digits in a Number
+
+#include <stdio.h>
+
+int main() {
+    int num, digit, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        digit = num % 10;
+        if (digit % 2 != 0) {
+            sum += digit;
+        }
+        num /= 10;
+    }
+
+    printf("Sum of odd digits = %d\n", sum);
+    return 0;
+}
