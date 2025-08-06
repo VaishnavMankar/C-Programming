4434eedde0c0a65ef4464a0c21945c035ee98ec4
Vaishnav Mankar <vmankar2910@gmail.com>
2025-08-06 08:41:12 +0530

    Calculate the Sum of Squares of First N Natural Numbers

---
+++ Practice Problem/4_Loops/DoWhile/DoWhile_10.c
@@ -0,0 +1,16 @@
+// 10. Calculate the Sum of Squares of First N Natural Numbers
+#include <stdio.h>
+
+int main() {
+    int N, sum = 0, i = 1;
+    printf("Enter N: ");
+    scanf("%d", &N);
+
+    do {
+        sum += i * i;
+        i++;
+    } while (i <= N);
+
+    printf("Sum of squares = %d\n", sum);
+    return 0;
+}
