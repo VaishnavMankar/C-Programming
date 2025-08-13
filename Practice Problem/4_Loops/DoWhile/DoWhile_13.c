5816722ee79bf938cbc34899e1a674ae9d7106ae
Vaishnav Mankar <vmankar2910@gmail.com>
2025-08-13 07:59:40 +0530

    Read 5 integers from users and perform addition and multiplication.

---
+++ Practice Problem/4_Loops/DoWhile/DoWhile_13.c
@@ -0,0 +1,20 @@
+// 13. Read five integers from the user and perform their addition, and multiplication user for loop
+
+#include <stdio.h>
+
+int main() {
+    int arr[5], i = 0, sum = 0, product = 1;
+
+    do {
+        printf("Enter integer %d: ", i + 1);
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+        product *= arr[i];
+        i++;
+    } while (i < 5);
+
+    printf("Sum = %d\n", sum);
+    printf("Product = %d\n", product);
+
+    return 0;
+}
