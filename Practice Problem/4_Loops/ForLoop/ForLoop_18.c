8c182c2f0261b04bda3ff664fde3d8adf552f7bc
Vaishnav Mankar <vmankar2910@gmail.com>
2025-08-27 11:19:24 +0530

    Read a number from the user and generate a multiplication table

---
+++ Practice Problem/4_Loops/ForLoop/ForLoop_18.c
@@ -0,0 +1,16 @@
+// 19. Read a number from the user and generate a multiplication table
+
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Multiplication table of %d:\n", num);
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
