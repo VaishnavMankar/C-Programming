028260018fc34deae9e4ea86dbb0d8f311fae497
Vaishnav Mankar <vmankar2910@gmail.com>
2025-09-09 22:31:56 +0530

    Read a number from the user and generate a multiplication table

---
+++ Practice Problem/4_Loops/DoWhile/DoWhile_19.c
@@ -0,0 +1,16 @@
+// 19. Read a number from the user and generate a multiplication table
+
+#include <stdio.h>
+
+int main() {
+    int num, i = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    do {
+        printf("%d x %d = %d\n", num, i, num * i);
+        i++;
+    } while (i <= 10);
+
+    return 0;
+}
